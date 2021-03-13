7e12da6417ca5b24497b9f3422eb601bca3faa21
rebeccapatek <rebeccapatek@gmail.com>
2021-03-13 09:47:11 -0600

    first commit of intro exercise

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+namespace Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your sign? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! I'm figuring out your chart to see who you're meant to be with. Please wait.");
+            var milliseconds = 3000;
+            Thread.Sleep(milliseconds);
+            Console.WriteLine($"...");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine($"Your soul-mate is William Shatner.");
+        }
+    }
+}
